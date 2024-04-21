5906ee3128f4654d2260f332c9c6d3e2eae0b03d
Staś <Sta@KOMPUTER-STAS>
2024-04-21 12:58:02 +0200

    Repair Quiz 1.0

--- RepairQuiz/RepairQuiz.js
+++ RepairQuiz/RepairQuiz.js
@@ -54,26 +54,72 @@ class nRepairQuiz {
 		["approve",1],["adequate",2],["responsible",5],["legal",4],
 		["probable",3],["mature",3],["safe",0]]
 	questionIndex = -1
+	correctCount = 0
 	start() {
 		this.qelem = document.getElementById("pqTemplate");
 		this.main = document.getElementsByTagName("main")[0];
+		//this.field = QuestionField(this.questions[0][0])
 		this.nextQuestion();
 	}
 	nextQuestion() {
 		this.questionIndex++;
-		let nn = this.qelem.cloneNode(true);
-		nn.children[0].innerHTML = this.questions[this.questionIndex][0];
-		this.main.appendChild(nn);
+		if (this.questionIndex < this.questions.length) {
+			//this.field.header = this.questions[this.questionIndex][0]
+			let nn = this.qelem.cloneNode(true);
+			nn.children[0].innerHTML = this.questions[this.questionIndex][0];
+			this.main.appendChild(nn);
+		}
+		else {
+			let el = document.getElementById("finshMessage");
+			let counters = el.getElementsByTagName("span");
+			counters[0].innerText = this.correctCount;
+			counters[1].innerText = this.questions.length;
+			this.main.append(el)
+		}
 	}
 	answer(ans, elem) {
 		let correct = this.questions[this.questionIndex][1];
-		if (ans == correct)
+		if (ans == correct) {
 			elem.classList.add("good");
+			this.correctCount++;
+		}
 		else
 			elem.classList.add("bad");
-		elem.children[2+correct/3].children[correct%3].classList.add("correct");
+		elem.children[2+Math.trunc(correct/3)].children[correct%3].classList.add("correct");
 		this.nextQuestion()
-		setTimeout(()=>{elem.remove()},1990)
+		setTimeout(()=>{elem.remove()},1950)
+	}
+	goodAnswer() {}
+	badAnswer() {
+
+	}
+}
+var QuestionField = function(question, answers, correct, parent) {
+	let elem = document.createElement("div");
+	elem.classList.add("pinquestion");
+
+	let headerElem = document.createElement("h3");
+	headerElem.innerText=question;
+
+	let inputElem = document.createElement("input");
+	inputElem.type = "password";
+
+	let answersElems = []
+	for (let i = 0; i < answers.length; i++) {
+		let el = document.createElement("button");
+		if (i === correct)
+			el.onclick = parent.goodAnswer()
+		else
+			el.onclick = parent.badAnswer()
+	}
+
+	elem.appendChild(headerElem);
+	elem.appendChild(inputElem);
+	document.body.appendChild(elem);
+	return {
+		set header(newHeader) {
+			headerElem.innerText = newHeader;
+		},
 	}
 }
 
